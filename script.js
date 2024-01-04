0d2e30000576af8ffdc6a75c339db2b55c4cfde1
Simone Penido <simone_penido@hotmail.com>
2024-01-04 00:17:44 -0300

    Update

--- script.js
+++ script.js
@@ -1,9 +1,19 @@
-let time = 5000,
+/**
+ * O intervalo de tempo para a troca de imagens em milissegundos.
+ * @type {number}
+ */
+let time = 4000,
     currentImageIndex = 0,
     images = document
                 .querySelectorAll("#slider img")
     max = images.length;
 
+/**
+ * Move para a próxima imagem na galeria de imagens.
+ */
+/**
+ * Moves to the next image in the image gallery.
+ */
 function nextImage() {
 
     images[currentImageIndex]
@@ -18,9 +28,12 @@ function nextImage() {
         .classList.add("selected")
 }
 
+/**
+ * Inicia o intervalo de troca de imagem.
+ */
 function start() {
     setInterval(() => {
-        // troca de image
+        // troca de imagem
         nextImage()
     }, time)
 }
